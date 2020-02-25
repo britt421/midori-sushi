b9385a3a85809ef7721c2eb03f6c13c482ec539d
Brittany Taylor <hopkins.britt@gmail.com>
2020-02-25 13:48:12 -0600

    initial testing env

---
+++ assets/logic.js
@@ -0,0 +1,31 @@
+var arrExclude = [];
+var arrInclude = [];
+
+function sortByIngredient(arr) {
+    if(arr.ingredients.indexOf("cucumber") > -1){
+        arrExclude.push(arr.name);
+    } else {
+        arrInclude.push(arr.name);
+    }
+}
+
+sortByIngredient(midoriRolls);
+
+
+
+// var mySpicyRolls = midoriRolls.filter(function(rolls){
+//     return rolls.spicy == true;
+// });
+
+// var myRolls = midoriRolls.filter(function(rolls){
+//     var mySpecialRolls = [];
+//     if (rolls.ingredients.indexOf("cucumber") > -1){
+//     mySpecialRolls.push(rolls.name);
+//     } else {
+//         console.log("There are no rolls that match your search criteria")
+//     }
+//     console.log(mySpecialRolls);
+// });
+
+// console.log(myRolls);
+// console.log(mySpicyRolls);
---
+++ assets/rolls.js
@@ -0,0 +1,45 @@
+var midoriRolls = [
+
+    {name: "California Roll",
+    ingredients: ["crab meat", "cucumber", "avocado"],
+    wrapper: "seaweed",
+    spicy: false,
+    raw: false,
+    price: 5.50},
+
+    {name: "Tekka Maki (Tuna Roll)",
+    ingredients: ["tuna"],
+    wrapper: "seaweed",
+    spicy: false,
+    raw: true,
+    price: 5.50},
+
+    {name: "Spicy Tuna Roll",
+    ingredients: ["spicy tuna", "cucumber", "radish sprout"],
+    wrapper: "seaweed",
+    spicy: true,
+    raw: true,
+    price: 7.50},
+
+    {name: "Spicy Crunch Roll",
+    ingredients: ["spicy crab meat", "cucumber", "tempura flakes"],
+    wrapper: "seaweed",
+    spicy: true,
+    raw: true,
+    price: 7.00},
+
+    {name: "Yellowtail Roll",
+    ingredients: ["yellowtail", "cucumber", "scallion"],
+    wrapper: "seaweed",
+    spicy: false,
+    raw: true,
+    price: 7.50},
+    
+    {name: "Spicy Yellowtail Roll",
+    ingredients: ["spicy yellowtail", "cucumber", "scallion", "radish sprout"],
+    wrapper: "seaweed",
+    spicy: true,
+    raw: true,
+    price: 8.00}
+
+]
